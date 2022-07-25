28de833580e439155758daaca0e21bc5e6009f13
YuanboXue-Amber <yuanboxue@microsoft.com>
2022-07-25 22:28:35 +0200

    App.styles.js show up in webpack chunk obj

--- packages/test2/src/App.js
+++ packages/test2/src/App.js
@@ -4,8 +4,12 @@ import { useHeroStyles } from '../../test1/src/App.styles';
 function App() {
   const heroClasses = useHeroStyles();
   return React.createElement(
-    'div',
-    { className: heroClasses.root },
+    'a',
+    {
+      className: heroClasses.root,
+      href: undefined,
+      // href: 'vscode://file/Users/yuanboxue/dev/fluent/griffel/g2/griffel/packages/devtools/src/SlotCSSRules.tsx:5:10',
+    },
     'hi'
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,36 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve('./packages/test2/src/index.js'),
-  resolve: {
-    extensions: ['*', '.js'],
+  mode: 'development',
+  entry: {
+    index: {
+      import: path.resolve('./packages/test2/src/index.js'),
+      dependOn: 'shared',
+    },
+    amberStyle: {
+      import: path.resolve('./packages/test1/src/App.styles.js'),
+      dependOn: 'shared',
+    },
+    shared: 'react',
   },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
     // uniqueName: '@testscope/testname',
   },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
   devServer: {
     static: path.resolve(__dirname, './dist'),
   },
-  optimization: {
-    minimize: false,
-    runtimeChunk: false,
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
+  // optimization: {
+  //   // minimize: false,
+  //   // runtimeChunk: false,
+  //   splitChunks: {
+  //     chunks: 'all',
+  //   },
+  // },
   devtool: 'eval-cheap-module-source-map',
   plugins: [
     new HtmlWebpackPlugin({
